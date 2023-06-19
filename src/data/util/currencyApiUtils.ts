4e6a5748d30b46346d1d2050e0962d648a192c60
susosamuel <samuel.susoliak@joinswapp.com>
2023-06-19 15:59:26 +0200

    jest introduction

--- src/data/util/currencyApiUtils.ts
+++ src/data/util/currencyApiUtils.ts
@@ -4,5 +4,7 @@ import { ExchangeRateRequest } from '../types/ExchangeRateRequest.ts'
 
 export const API_URL = `${CURRENCY_API_BASE_URL}?apikey=${CURRENCY_API_KEY}`
 
-export const getCurrencyParams = ({ baseCurrency, exchangeCurrencies }: ExchangeRateRequest) =>
-    `base_currency=${baseCurrency}&currencies=${exchangeCurrencies.join('%2C')}`
+export const getCurrencyParams = ({ baseCurrency, exchangeCurrencies }: ExchangeRateRequest) => {
+    if (exchangeCurrencies.length === 0) throw 'Invalid argument'
+    return `base_currency=${baseCurrency}&currencies=${exchangeCurrencies.join('%2C')}`
+}
